f5bbd27218a58fe3aa7951a8226db584aa7777f7
dieptuthinh <55689660+dieptuthinh@users.noreply.github.com>
2024-03-14 10:15:18 +0700

    Create useLocalStorage.ts
    useLocalStorage

---
+++ useLocalStorage.ts
@@ -0,0 +1,28 @@
+// Hook useLocalStorage 
+import { useState } from 'react';
+
+type SetValueFunction = (newValue: string) => void;
+type UseLocalStorageType = [string, SetValueFunction];
+
+export const useLocalStorage = (keyName: string, defaultValue: string = ''): UseLocalStorageType => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const value = localStorage.getItem(keyName);
+      if (value) {
+        return value;
+      } else {
+        localStorage.setItem(keyName, defaultValue);
+        return defaultValue;
+      }
+    } catch (err) {
+      return defaultValue;
+    }
+  });
+  const setValue = (newValue: string) => {
+    try {
+      window.localStorage.setItem(keyName, newValue);
+    } catch (err) {}
+    setStoredValue(newValue);
+  };
+  return [storedValue, setValue];
+};
