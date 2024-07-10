cd511ce04d48a36f33caa3ad7f8c4cfeb1a40012
dieptuthinh <55689660+dieptuthinh@users.noreply.github.com>
2024-07-10 09:36:38 +0700

    Update LearnJava.java

--- LearnJava.java
+++ LearnJava.java
@@ -2,3 +2,7 @@ String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 for (String i : cars) {
   System.out.println(i);
 }
+System.out.println(cars[0]);
+
+int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+System.out.println(myNumbers[1][2]); // Outputs 7
